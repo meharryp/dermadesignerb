e6b0d97b6cbfe5a041381119591e7994de9bdae1
phil.auriemma <phil.auriemma@08ab09da-3626-4f70-d894-e26de802b6c1>
2010-05-31 19:44:36 +0000

    fixed drag-and-drop-from-a-different-directory bug

--- trunk/DermaDesigner/DermaDesigner/Base/DPacker/DSave.cs
+++ trunk/DermaDesigner/DermaDesigner/Base/DPacker/DSave.cs
@@ -6,94 +6,76 @@ using System.Text;
 using System.Windows.Forms;
 
 
-namespace DermaDesigner
-{
-    /// <summary>
-    /// A class to handle the saving and loading environment
-    /// </summary>
-    public class DSave
-    {
-        private static string CurrentFilename = "Untitled.ddproj";
-        private static DPacker Packer = new DPacker();
-        
-        public static void SetEnvironment(string filename)
-        {
-            CurrentFilename = filename;
-            Derma.GetWorkspace().Text = "DermaDesigner - " + Path.GetFileName(filename);
-        }
+namespace DermaDesigner {
+	/// <summary>
+	/// A class to handle the saving and loading environment
+	/// </summary>
+	public class DSave {
+		private static string CurrentFilename = "Untitled.ddproj";
+		private static DPacker Packer = new DPacker();
 
-        public static void SetDialogDefaults()
-        {
-            GetOpenDialog().FileName = Path.GetFileName(CurrentFilename);
-            GetSaveDialog().FileName = Path.GetFileName(CurrentFilename);
-        }
-        public static string GetEnvironment()
-        {
-            return CurrentFilename;
-        }
-        
-        public static OpenFileDialog GetOpenDialog()
-        {
-            return ((Main) Derma.GetWorkspace()).OpenDialog;
-        }
+		public static void SetEnvironment(string filename) {
+			CurrentFilename = filename;
+			Derma.GetWorkspace().Text = "DermaDesigner - " + Path.GetFileName(filename);
+		}
 
-        public static SaveFileDialog GetSaveDialog()
-        {
-            return ((Main)Derma.GetWorkspace()).SaveDialog;
-        }
+		public static void SetDialogDefaults() {
+			GetOpenDialog().FileName = Path.GetFileName(CurrentFilename);
+			GetSaveDialog().FileName = Path.GetFileName(CurrentFilename);
+		}
+		public static string GetEnvironment() {
+			return CurrentFilename;
+		}
 
-        public static void Save()
-        {
-            try
-            {
-                Packer.PackAllToFile(CurrentFilename);
-            }catch(Exception e)
-            {
-                MessageBox.Show("Unable to save file\n" + e.Message, "Unable to save", MessageBoxButtons.OK,
-                                MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
-                return;
-            }
-        }
+		public static OpenFileDialog GetOpenDialog() {
+			return ((Main)Derma.GetWorkspace()).OpenDialog;
+		}
 
-        public static void SaveAs(string filename)
-        {
-            SetEnvironment(filename);
-            Save();
-        }
+		public static SaveFileDialog GetSaveDialog() {
+			return ((Main)Derma.GetWorkspace()).SaveDialog;
+		}
 
-        public static void ClearAll()
-        {
-            SetEnvironment("Untitled.ddproj");
-            List<Panel> PList = new List<Panel>();
-            PList.Concat(Derma.GetPanels());
-            foreach(Panel P in PList)
-            {
-                try
-                {
-                    P.Remove();
-                }catch
-                {
-                    // We don't care if it doesn't want to remove, the panel list will get cleared anyways.
-                }
-            }
-            Derma.GetPanels().Clear();
-            ResizeGrip.host = null;
-            Derma.Repaint();
-        }
+		public static void Save() {
+			try {
+				Packer.PackAllToFile(CurrentFilename);
+			} catch (Exception e) {
+				MessageBox.Show("Unable to save file\n" + e.Message, "Unable to save", MessageBoxButtons.OK,
+								MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+				return;
+			}
+		}
 
-        public static void Load(string filename)
-        {
-            try
-            {
-                Packer.ReadAllFromFile(filename);
-            }catch(Exception e)
-            {
-                MessageBox.Show("Unable to load file\n" + e.Message, "Unable to load", MessageBoxButtons.OK,
-                               MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
-                return;
-            }
-            SetEnvironment(filename);
-            Derma.Repaint();
-        }
-    }
+		public static void SaveAs(string filename) {
+			SetEnvironment(filename);
+			Save();
+		}
+
+		public static void ClearAll() {
+			SetEnvironment("Untitled.ddproj");
+			List<Panel> PList = new List<Panel>();
+			PList.Concat(Derma.GetPanels());
+			foreach (Panel P in PList) {
+				try {
+					P.Remove();
+				} catch {
+					// We don't care if it doesn't want to remove, the panel list will get cleared anyways.
+				}
+			}
+			Derma.GetPanels().Clear();
+			ResizeGrip.host = null;
+			Derma.Repaint();
+		}
+
+		public static void Load(string filename) {
+			try {
+				Packer.ReadAllFromFile(filename);
+			} catch (Exception e) {
+				MessageBox.Show("Unable to load file\n" + e.Message, "Unable to load", MessageBoxButtons.OK,
+							   MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+				return;
+			}
+			SetEnvironment(filename);
+			Derma.Repaint();
+		}
+	}
 }
